41ed6c08238d8f99e1f60d3af5e6e83a3d0dc4b7
Glenald Gressmann <105360252+GlenaldGressmann@users.noreply.github.com>
2024-12-04 23:01:38 +0100

    -

--- sjoelmaster/routes/web.php
+++ sjoelmaster/routes/web.php
@@ -2,11 +2,12 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\testcontroller;
+use App\Http\Controllers\sjoelmaster;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::resource('scores', sjoelmaster::class);
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -16,5 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::get('/test', [testcontroller::class, 'test'])->name('test');
+Route::get('/home', [sjoelmaster::class, 'home'])->name('home');
 
 require __DIR__.'/auth.php';
+
